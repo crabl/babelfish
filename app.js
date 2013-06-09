58bd51dc6640159d7cba1c6a52d183962b151d7f
Chris Rabl <rabl.christopher@gmail.com>
2013-06-09 04:36:47 -0400

    First Commit

---
+++ app.js
@@ -0,0 +1,30 @@
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , http = require('http')
+  , path = require('path');
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 8080);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res){
+    res.render('index', { title: '' });
+});
+
+app.post('/compile', function(req, res){
+    
+});
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
