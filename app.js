31a44f74c77be69adb4c9277d7c0d3153e5c009f
Chris Rabl <rabl.christopher@gmail.com>
2013-06-09 12:21:22 +0000

    Added Emscripten

--- app.js
+++ app.js
@@ -3,8 +3,13 @@
  */
 
 var express = require('express')
-  , http = require('http')
-  , path = require('path');
+, http = require('http')
+, fs = require('fs')
+, crypto = require('crypto')
+, sys = require('sys')
+, buf = require('buffer')
+, exec = require('child_process').exec
+, path = require('path');
 
 var app = express();
 
@@ -17,12 +22,40 @@ app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function(req, res){
-    res.render('index', { title: '' });
-});
+function emscripten_compile(fileName, callback) {
+    child = exec("server/emscripten/emcc /tmp/"+fileName+".cpp -o /tmp/"+fileName+".js", function (error, stdout, stderr) {
+	sys.print('stdout: ' + stdout);
+	sys.print('stderr: ' + stderr);
+	if (error !== null) {
+	    console.log('exec error: ' + error);
+	} else {
+	    callback(fileName);
+	}
+    });
+}
 
-app.post('/compile', function(req, res){
-    
+app.post('/cpp', function(req, res){
+    console.log("Got file from client:");
+    console.log(req.param('script'));
+    var fileName = crypto.randomBytes(16).toString('hex');
+    fs.writeFile("/tmp/"+fileName+".cpp", req.param("script"), function(e) {
+	if(e) {
+	    console.log(e);
+	    res.send(500, "FAIL");
+	} else {
+	    console.log("Wrote file");
+	    emscripten_compile(fileName, function(o) {
+		if(o == undefined) {
+		    console.log(e);
+		    res.send(500, "FAIL");
+		} else {
+		    console.log("Sending File...");
+		    res.sendfile("/tmp/"+fileName+".js");
+		    console.log("DONE");
+		}
+	    });
+	}
+    });
 });
 
 http.createServer(app).listen(app.get('port'), function(){
